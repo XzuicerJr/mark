3f139d2dd21dff8e2e3aef4e4293c8a1c3f6de70
César Zamora <zuicerjr24@gmail.com>
2025-07-07 23:01:43 -0600

    (feat): api endpoint - get all habits

--- app/api/habits/route.ts
+++ app/api/habits/route.ts
@@ -1,9 +1,19 @@
 import { ApiError } from "@/lib/api/errors";
+import { getHabits } from "@/lib/api/habits/get-habits";
 import { parseRequestBody } from "@/lib/api/utils";
 import { withSession } from "@/lib/auth/session";
 import { createHabitBodySchema } from "@/lib/zod/schema/habits";
 import { prisma } from "@/prisma";
 import { NextResponse } from "next/server";
+
+// GET /api/habits – get all habits for a user
+export const GET = withSession(async ({ session }) => {
+  const response = await getHabits({
+    userId: session.user.id,
+  });
+
+  return NextResponse.json(response);
+});
 
 // POST /api/habits – create a new habit
 export const POST = withSession(async ({ session, req }) => {
